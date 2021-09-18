79218ca867d69418304cb064e2f29c1fa490e611
Emma HB <emmanuelle.bolognini@gmail.com>
2021-09-18 17:24:29 +0200

    Itération 1 > computeTwoDigitNumber(value)

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,22 +1,29 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    setInterval(() => { 
+      this.currentTime += 1;
+    },1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (this.getSeconds.toString().length = 1 ) {
+      return (`0${this.getSeconds.toString()}`)
+    } else {
+      return this.getSeconds.toString();
+    }; 
   }
 
   stop() {
