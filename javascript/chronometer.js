f3126af3b4fc7b61e10816857816a3d8c645584a
kthomas <kevin.allan.thomas@gmail.com>
2021-09-20 11:56:41 +0200

    latest updates

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -15,18 +15,35 @@
   }
 
   getMinutes() {
+    var secondstoMin = this.currentTime % (60 * 60);
+    return secondstoMin = Math.floor (secondstoMin / 60);
     // ... your code goes here
   }
 
   getSeconds() {
+    var seconds = this.currentTime % 60;
+    return seconds;
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+    var minute = this.getMinutes;
+    var second = this.getSeconds;
+    var value = minute + second;
+    if (second >= 60){
+      var m = (second / 60) << 0
+      minute += m
+      second -= 60 * m
+    }else{
+    return ;
+    }
+    return value.String().slice(-2);
     // ... your code goes here
   }
 
   stop() {
+
+    clearInterval()
     // ... your code goes here
   }
 
