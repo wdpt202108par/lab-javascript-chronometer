f586296430857a1ac71cbce82bbf18028afa9515
lea-beaulieu <87803489+lea-beaulieu@users.noreply.github.com>
2021-09-21 16:25:48 +0200

    added reset + split, check consistency of clearInterval but all controls are OK

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -6,6 +6,7 @@ class Chronometer {
   }
 
   start(callback) {
+    // Interval of 1 sec
     this.intervalId= setInterval(() => {
       this.currentTime++;
       if(callback) callback()
@@ -18,12 +19,15 @@ class Chronometer {
   }
 
   getSeconds() {
+    // Modulo reste de la division par 60 secondes
     return Math.floor(this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
+    // si juste un chiffre alors rajouter un 0 devant sous format string
     if (value<10) {
       return "0"+value;
+      // sinon simplement convertir le nombre sous format string et c'est good
     } else {
       return `${value}`
     }
@@ -32,14 +36,27 @@ class Chronometer {
 
 
   stop() {
+    /* The clearInterval() method clears a timer set with the setInterval() method.
+    The ID value returned by setInterval() is used as the parameter for the clearInterval() method.
+    Note: To be able to use the clearInterval() method, you must use a variable when creating the interval method: */
+    
+    clearInterval(this.intervalId+=null);
     // ... your code goes here
   }
 
   reset() {
+    //"we just need to set our currentTime" @Julie Going back to 0 ?
+    this.currentTime=0;
     // ... your code goes here
   }
 
   split() {
+    // creation var pour retriever minutes et seconds au format chrono (2 digits) 
+    let minutesTwoDigits= this.computeTwoDigitNumber(this.getMinutes());
+    let secondsTwoDigits = this.computeTwoDigitNumber(this.getSeconds());
+
+    //retourner valeur string format chrono
+    return `${minutesTwoDigits}:${secondsTwoDigits}`;
     // ... your code goes here
   }
 }
