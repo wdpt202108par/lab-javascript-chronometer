7c3664a8e417e89ae3e9f72b7c10c1d06fbe1f8b
frenchToast <juliekogan1@gmail.com>
2021-09-23 20:32:36 +0200

    wip

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-const chronometer = new Chronometer();
+const chronometer = new Chronometer(); // {currentTime: 0, intervalId: null}
 
 // get the buttons:
 const btnLeftElement = document.getElementById('btnLeft');
@@ -14,7 +14,25 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  // exec toutes les 1s
+
+  // recupere chronometer.split() 02:36"
+
+  const splittedTime = chronometer.split()
+  console.log(splittedTime.charAt(4))
+
+  const a = splittedTime.charAt(0)
+  const b = splittedTime.charAt(1)
+  const c = splittedTime.charAt(3)
+  const d = splittedTime.charAt(4)
+
+  // maj nos 4 spans avec 
+  minDecElement.innerHTML = a; // "02:36"
+  minUniElement.innerHTML = chronometer.split().chartAt(1);
+  //secDecElement.innerHTML = chronometer.split().chartAt(3);
+  //secUniElement.innerHTML = chronometer.split().chartAt(4);
+  
+
 }
 
 function printMinutes() {
@@ -39,7 +57,7 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-  // ... your code goes here
+  setStopBtn = btnLeftElement.innerHTML// ... your code goes here
 }
 
 function setSplitBtn() {
@@ -47,7 +65,7 @@ function setSplitBtn() {
 }
 
 function setStartBtn() {
-  // ... your code goes here
+  setStartBtn = btnLeftElement.innerHTML// ... your code goes here
 }
 
 function setResetBtn() {
@@ -56,8 +74,25 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
-});
+  if (btnLeftElement.innerHTML === "START") {
+    chronometer.start(printTime)
+
+  
+    //
+    // btn left
+    //
+    
+    btnLeftElement.className = 'btn stop' // bouton en rouge
+    btnLeftElement.innerHTML = "STOP"// START -> STOP
+
+    
+    // btn right
+    //
+
+  } else {
+    chronometer.stop()
+  }
+})
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
